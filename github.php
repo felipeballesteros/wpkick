125ed1f0596f0051b3e100e367a079ae93f0f982
felipeb <felipeb@criticalmass.com>
2013-06-22 09:47:29 -0400

    Tryout with page as a template

--- github.php
+++ github.php
@@ -1,1 +1,35 @@
- <?php `git pull`; 
+<?php
+/*
+Template Name: Github Pull
+*/
+?>
+
+ <?php `git pull`; ?>
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<title>Github pull</title>
+
+	<style>
+		#container{
+			width:450px;
+			margin:30px auto;
+		}
+
+		p{
+			font-family: Verdana;
+			font-color:#999;
+			font-size:13px;
+		}
+
+	</style>
+
+</head />
+<body>
+	<div id="container">
+		<p>This page will make an automatic pull from the Git repo, use only when code is ready to go on production</p>
+	</div>
+</body>
+</html>
+
+
